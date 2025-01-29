f200b37270b6ee3a19086f51bafc078a7a1885ed
Rikiub <rikiub.dev@gmail.com>
2025-01-29 14:52:57 -0400

    Yes

---
+++ src/routes/+page.svelte
@@ -0,0 +1,14 @@
+<script>
+    import Rain from "$lib/Rain.svelte";
+</script>
+
+<main>
+    <Rain />
+</main>
+
+<style>
+    :global(body) {
+        margin: 0;
+        background-color: #1a1a1a;
+    }
+</style>
